96f59f59b115deb4914998aa91d15b26934871fd
t-yamanashi <ae86bg5@yahoo.co.jp>
2021-09-29 15:48:11 +0900

    Add github_issue

--- elixir/issues/lib/issues/cli.ex
+++ elixir/issues/lib/issues/cli.ex
@@ -2,9 +2,10 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
-
   def parse_args(argv) do
     OptionParser.parse(argv,
       switches: [help: :boolean],
@@ -20,4 +21,15 @@ defmodule Issues.CLI do
   def args_to_internal_representiation([user, project]), do: {user, project, @default_count}
 
   def args_to_internal_representiation(_), do: :help
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
 end
