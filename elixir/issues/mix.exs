cfd91892f6c73451f9a9cd6f25c9f680c9a6ebaa
t-yamanashi <ae86bg5@yahoo.co.jp>
2021-09-29 16:07:32 +0900

    Fix mix.exs :add posion

--- elixir/issues/mix.exs
+++ elixir/issues/mix.exs
@@ -21,7 +21,8 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.0.0"}
+      {:httpoison, "~> 1.0.0"},
+      {:poison, "~> 3.1"},
     ]
   end
 end
